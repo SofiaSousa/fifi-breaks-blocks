f499920744f3401b0bbbba0185415f0b46a8acb8
Sofia Sousa <csofiamsousa@gmail.com>
2020-07-24 17:03:58 +0100

    feat: Add new built app

--- main-build.rb
+++ main-build.rb
@@ -50,8 +50,9 @@ class Weapon
   end
 end
 
+# The game hero
 class Hero
-  attr_reader :shots
+  attr_reader :body, :shots
 
   def initialize
     @width = 150
@@ -94,6 +95,10 @@ class Hero
     @body.x += @x_speed
   end
 end
+# A block object to destroy that can have one of these 3 colors:
+# - yellow : 1 shot to destroy
+# - orange : 2 shots to destroy
+# - red    : 3 shots to destroy
 class Block
   attr_reader :body, :status
 
@@ -142,7 +147,8 @@ class Block
   end
 end
 
-class Line
+# A raw of blocks
+class Raw
   attr_reader :blocks
 
   N_ITEMS = 10
@@ -163,65 +169,174 @@ class Line
   end
 end
 
-set title: 'Fifi is in the house',
+# The limit until blocks can go down before game over.
+class Limit
+  attr_reader :body
+
+  def initialize(opt = {})
+    @body = draw
+  end
+
+  def draw
+    y = Window.height - 170
+    Line.new(
+      x1: 0, y1: y,
+      x2: Window.width, y2: y,
+      width: 1,
+      color: 'silver',
+      z: 20
+    )
+  end
+end
+class Overlay
+  def initialize(opt = {})
+    @body = draw
+    @text_body = draw_text(opt[:text])
+    center_text
+    hide # hidden by defautl
+  end
+
+  def draw
+    rect = Rectangle.new(
+      x: 0,
+      y: 0,
+      width: Window.width,
+      height: Window.height,
+      color: 'silver',
+      z: 100
+    )
+
+    rect.opacity = 0.5
+    rect
+  end
+
+  def draw_text(text)
+    text = text || ''
+    Text.new(
+      text,
+      size: 60,
+      color: 'red',
+      z: 100
+    )
+  end
+
+  def show
+    @body.add
+    @text_body.add
+  end
+
+  def hide
+    @body.remove
+    @text_body.remove
+  end
+
+  def text=(new_text)
+    @text_body.text = new_text
+    center_text
+  end
+
+  private
+
+  def center_text
+    @text_body.x = (Window.width / 2) - (@text_body.width / 2)
+    @text_body.y = (Window.height / 2) - (@text_body.height / 2)
+  end
+end
+
+# World settings
+set title: 'Fifi Breaks Blocks!',
     background: 'navy',
     width: 640,
-    height: 640, # 1280,
+    height: 960,
     resizable: false
 
+# Init
+is_game_over = false
+is_paused    = false
 start_time = Time.now
 
 hero = Hero.new
+limit = Limit.new
+gover_overlay = Overlay.new(text: '- GAME OVER -')
+pause_overlay = Overlay.new(text: '- PAUSED -')
+score = 0
+score_bar = Text.new(
+  score,
+  color:
+  'white',
+  x: 20, y: Window.height - 25,
+  z: 20
+)
+
+# Add initial blocks
 blocks = []
-blocks += Line.new(y: 0).blocks
+blocks += Raw.new(y: 0).blocks
 blocks.each do |block|
   block.go_down
 end
-blocks += Line.new(y: 0).blocks
+blocks += Raw.new(y: 0).blocks
 
+# Events
 on :key_down do |event|
-  if event.key == 'escape'
-    close
-  end
+  close if event.key == 'escape'
 
-  if event.key == 'space'
-    hero.shoot
+  unless is_game_over
+    is_paused = ! is_paused if event.key == 'p'
+    hero.shoot if ! is_paused && event.key == 'space'
   end
 end
 
 on :key_held do |event|
-  if event.key == 'left' || event.key == 'right'
-    hero.move(event.key)
-  end
+  hero.move(event.key) if event.key == 'left' || event.key == 'right'
 end
 
+# Loop
 update do
-  if ((Time.now - start_time) * 1000) > 5000
-    # New line of blocks
-    blocks.each do |block|
-      block.go_down
+  if is_game_over
+    # gover_overlay.text = "GAME OVER - #{score}"
+    gover_overlay.show
+  elsif is_paused
+    pause_overlay.show
+    start_time = Time.now
+  else
+    pause_overlay.hide
+
+    if ((Time.now - start_time) * 1000) > 5000
+      # New line of blocks
+      blocks.each do |block|
+        block.go_down
+      end
+
+      blocks += Raw.new(y: 0).blocks
+
+      start_time = Time.now
     end
 
-    blocks += Line.new(y: 0).blocks
-
-    start_time = Time.now
-  end
-
-  # Shots
-  hero.shots.each do |shot|
-    # Blocks
     blocks.each do |block|
-      if block.collision_detected?(shot)
-        shot.hit(block)
+      if block.collision_detected?(limit)
+        is_game_over = true
+        hero.body.remove
+      end
 
-        hero.shots.delete(shot)
-        blocks.delete(block) if block.is_detroyed?
-        break
+      hero.shots.each do |shot|
+        if block.collision_detected?(shot)
+          shot.hit(block)
+          score += 10
+          score_bar.text = score
+
+          hero.shots.delete(shot)
+          blocks.delete(block) if block.is_detroyed?
+          break
+        end
       end
     end
 
-    shot.fly
+    # Shots
+    hero.shots.each do |shot|
+      shot.fly
+    end
   end
 end
 
+# Render
 show
